61bf8154314a7084846156d9972aa723b3cb61b1
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-03-27 16:33:56 +0530

    json file removed

--- src/Players-Cart/Players-Cart.js
+++ src/Players-Cart/Players-Cart.js
@@ -3,9 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Players-Cart.css";
 const PlayersCart = (props) => {
-  // const AddedToCart = () => {
-  //   console.log("clicked");
-  // };
   const { addToCart, player } = props;
   return (
     <div className="cart">
@@ -16,9 +13,6 @@ const PlayersCart = (props) => {
       <h4>
         Price : <i>${player.price}</i>
       </h4>
-      {/* <a className="btn" href="#">
-        <h6>ADD TO CART</h6>
-      </a> */}
       <button
         onClick={() => {
           addToCart(player);
